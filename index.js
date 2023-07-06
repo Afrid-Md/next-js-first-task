f50b5aa4ca23136226d4b4d4f23fa41ee89a8d20
Afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-07-06 15:33:02 +0530

    m3 is working

--- index.js
+++ index.js
@@ -11,4 +11,40 @@ function MeetupDetailsPage(){
         />
     )
 }
-export default MeetupDetailsPage;
+
+export async function getStaticPaths(){
+    return{
+        fallback : true,
+        paths : [
+            {
+                params :{
+                    meetupId : 'm1'
+                }
+            },
+            {
+                params :{
+                    meetupId : 'm2'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context){
+
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+    return{
+        props : {
+            meetupData : {
+                id:meetupId,
+                image :"https://upload.wikimedia.org/wikipedia/commons/7/74/Adelaide_%28AU%29%2C_Botanic_Garden_--_2019_--_0668.jpg", 
+                title:'A First meet up',
+                address:'Some street 12345, Some city',
+                description:'The meetup description'
+            }
+        }
+    }
+
+}
+export default MeetupDetailsPage ;
