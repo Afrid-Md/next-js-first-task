a0b619ff74bcd9841e951be40cb880f940c1a4f3
Afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-07-06 11:46:23 +0530

    added un-ordered list
    rendering list of name in about us page, when user clicks on it he will get specific user details

--- aboutus/page.js
+++ aboutus/page.js
@@ -1,8 +1,24 @@
 //our-domain.com/aboutus
 
-function AboutUsPage(){
-    return(
+import Link from "next/link";
+import { Fragment } from "react";
+
+function AboutUsPage() {
+  return (
+    <Fragment>
       <h1>The About us Page</h1>
-    )
-  }
-  export default AboutUsPage;
+      <ul>
+        <li>
+          <Link href='/aboutus/1'>yash</Link>
+        </li>
+        <li>
+          <Link href='/aboutus/2'>vaibhav</Link>
+        </li>
+        <li>
+          <Link  href='/aboutus/3'>suresh</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+export default AboutUsPage;
